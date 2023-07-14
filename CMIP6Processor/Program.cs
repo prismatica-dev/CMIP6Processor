3c91772f69a42ba63ea2c3fa7ca576794adb0f2a
lily-software <contact@lily.software>
2023-07-14 10:32:49 +0000

    exporting

--- CMIP6Processor/Program.cs
+++ CMIP6Processor/Program.cs
@@ -32,29 +32,49 @@ namespace CMIP6Processor {
             List<FileInfo> mags = GetMAGFiles(models);
 
             Dictionary<int, List<decimal>> annualData = new Dictionary<int, List<decimal>>();
+            Dictionary<int, decimal> annualMean = new Dictionary<int, decimal>();
 
             foreach (FileInfo file in mags) {
+                // prepare data
                 string data = File.ReadAllText(file.FullName);
                 data = data.Substring(data.IndexOf("YEARS"));
                 data = data.Substring(data.IndexOf("\n") + 1);
                 string[] bdata = data.Split('\n');
 
+                int minimumyear = int.MaxValue;
+                int maximumyear = int.MinValue;
+
+                // import data
                 foreach (string year in bdata) {
                     string y = year.TrimStart();
 
                     // get year
                     if (!char.IsDigit(y.ToCharArray()[0])) continue;
                     int ny = Convert.ToInt32(y.Substring(0, y.IndexOf(' ')));
-                    
+                    if (ny > maximumyear) maximumyear = ny;
+                    else if (ny < minimumyear) minimumyear = ny;
+
                     // get kelvin
                     y = y.Substring(y.IndexOf(ny.ToString() + ny.ToString().Length + 1)).TrimStart();
                     if (!char.IsDigit(y.ToCharArray()[0])) continue;
-                    double kelvin = Convert.ToDouble(y.Substring(0, y.IndexOf('+'))) * 100d;
+                    decimal kelvin = Convert.ToDecimal(y.Substring(0, y.IndexOf('+'))) * 100M;
 
+                    // append data
+                    if (annualData.ContainsKey(ny)) annualData[ny].Add(kelvin);
+                    else annualData.Add(ny, new List<decimal>(new decimal[] { kelvin })); }
 
-                }
-            }
-        }
+                // calculate average
+                foreach (int year in annualData.Keys) {
+                    decimal sum = 0; 
+                    foreach (decimal d in annualData[year]) sum += d;
+                    annualMean.Add(year, sum / annualData[year].Count); }
+
+                // export data
+                Console.WriteLine($"data processed. exporting period {minimumyear} until {maximumyear}.");
+                string[] aM = new string[annualMean.Count];
+                int i = 0;
+                foreach (int year in annualMean.Keys) aM[i] = annualMean[year].ToString();
+                File.WriteAllLines($"{Environment.CurrentDirectory}\\cmip6-export.txt", aM); }}
         
         static List<FileInfo> GetMAGFiles(DirectoryInfo Directory) {
             List<FileInfo> files = new List<FileInfo>();
